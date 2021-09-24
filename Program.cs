06a14f166b7853fab45c8118b03e5b44d5381232
ABHI <abhishekitte11@gmail.com>
2021-09-24 18:21:03 +0530

    [ABHISHEK] Added a frequency of word

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableandBST.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(6);
+            myMapNode.Add("1", "To");
+            myMapNode.Add("2", "be");
+            myMapNode.Add("3", "or");
+            myMapNode.Add("4", "not");
+            myMapNode.Add("5", "to");
+            myMapNode.Add("6", "be");
+            //Console.WriteLine("Getting values by using {0}:{1}","4",myMapNode.Get("4"));
+            myMapNode.Display();
+            Console.WriteLine(myMapNode.CountFrequency("be"));
+            Console.ReadLine();
+        }
+    }
+}
